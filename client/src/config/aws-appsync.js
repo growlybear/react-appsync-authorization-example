a23e008299190fbe215d39ebe445179d3c76d691
Michael Allan <Michael Allan>
2018-06-05 21:57:16 +1000

    Integrate AppSync with CRA app

---
+++ client/src/config/aws-appsync.js
@@ -0,0 +1,6 @@
+export default {
+  "graphqlEndpoint": "https://wawgenkcdfcgpi3nyagmgtdjsu.appsync-api.us-west-2.amazonaws.com/graphql",
+  "region": "us-west-2",
+  "authenticationType": "API_KEY",
+  "apiKey": "da2-f3k5iriinfaepog5pp27swysui"
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Client from 'aws-appsync'
+import { Rehydrated } from 'aws-appsync-react'
+import { ApolloProvider as Provider } from 'react-apollo'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './App';
+import config from './config/aws-appsync'
+
+import './index.css';
+
+const client = new Client({
+  url: config.graphqlEndpoint,
+  region: config.region,
+  auth: {
+    type: config.authenticationType,
+    apiKey: config.apiKey
+  }
+})
+
+const AppWithProvider = () => (
+  <Provider client={client}>
+    <Rehydrated>
+      <App />
+    </Rehydrated>
+  </Provider>
+)
+
+ReactDOM.render(
+  <AppWithProvider />,
+  document.getElementById('root')
+);
