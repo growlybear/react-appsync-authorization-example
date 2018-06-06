a51aa5817c512886dbc1277b2a6f12cbc3b911cc
Michael Allan <Michael Allan>
2018-06-06 12:16:00 +1000

    Add handlers for new recipes and directions, and enable multiple graphql operations

--- client/src/App.js
+++ client/src/App.js
@@ -8,8 +8,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    name: '',
+    ingredient: '',
+    ingredients: [],
+    direction: '',
+    directions: []
+  }
+  onChange = (key, value) => {
+    this.setState({ [key]: value })
+  }
+  addIngredient = () => {
+    const ingredient = this.state.ingredient
+    // prevent empty ingredients from being added to array
+    if (ingredient) {
+      this.setState({
+        ingredients: [...this.state.ingredients, ingredient],
+        ingredient: ''
+      })
+    }
+  }
+  addDirection = () => {
+    const direction = this.state.direction
+    // prevent empty directions from being added to array
+    if (direction) {
+      this.setState({
+        directions: [...this.state.directions, direction],
+        direction: ''
+      })
+    }
+  }
   render() {
-    console.log('Props:', this.props)
     return (
       <div className="App">
         <header className="App-header">
@@ -29,11 +58,13 @@ class App extends Component {
   }
 }
 
-export default graphql(ListRecipes, {
-  options: {
-    fetchPolicy: 'cache-and-network',
-  },
-  props: props => ({
-    recipes: props.data.listRecipes ? props.data.listRecipes.items : []
+export default compose(
+  graphql(ListRecipes, {
+    options: {
+      fetchPolicy: 'cache-and-network',
+    },
+    props: props => ({
+      recipes: props.data.listRecipes ? props.data.listRecipes.items : []
+    })
   })
-})(App);
+)(App);
